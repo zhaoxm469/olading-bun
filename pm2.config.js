345455d3e803f5dbcb1dc1cc2d1f7c05b775e97d
zhaodage <zhaoxm469@163.com>
2025-07-15 09:10:18 +0800

    优化logs 日志页面

--- pm2.config.js
+++ pm2.config.js
@@ -1,15 +1,15 @@
 // pm2.config.js
 module.exports = {
     apps: [
-      {
-        name: 'olading-bun',
-        script: 'src/index.ts',
-        interpreter: 'bun',
-        watch: true, // 或者 false，根据需要
-        node_args: '--watch', // 或者 '--hot'
-        env: {
-          NODE_ENV: 'production', // 或者 'development'
+        {
+            name: 'olading-bun',
+            script: 'src/index.ts',
+            interpreter: 'bun',
+            watch: true, // 或者 false，根据需要
+            node_args: '--watch', // 或者 '--hot'
+            env: {
+                NODE_ENV: 'production', // 或者 'development'
+            },
         },
-      },
     ],
-  };
+};
